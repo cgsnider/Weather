b75e1b360240b8a86b12c8a8ffb0eb25c3b59bca
CGSnider <cgsnider@outlook.com>
2022-08-30 20:45:23 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Charles Snider on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
